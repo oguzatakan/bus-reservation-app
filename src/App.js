ae5c4b95d1eb7035b4957ee224297274160f461a
Atakan <atakan17171717@hotmail.com>
2022-05-14 12:47:02 +0300

    routes created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import TicketScreen from "./pages/TicketScreen/TicketScreen";
+import TravelPanel from "./pages/TravelPanel/TravelPanel";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/ticket" element={<TicketScreen />}>
+          <Route path=":travel" element={<TravelPanel />} />
+        </Route>
+        <Route path="/travelPanel" element={<TravelPanel />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
