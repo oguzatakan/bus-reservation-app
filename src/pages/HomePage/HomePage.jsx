8487811637654bf5505cc2ad7e85afb63fd25cfb
Atakan <atakan17171717@hotmail.com>
2022-05-14 13:56:59 +0300

    css updated

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,8 +5,24 @@ import "./HomePage.css";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="home-div">
+      <div className="admin-div">
+        <img
+          className="admin-img"
+          src="https://pos.bakiyem.com/surucuimg/0C6299908F669B21AA05485DC74D8D05.jpeg"
+        />
+      </div>
+
+      <div className="project-name-div">
+        <h1>Otobus Bilet Projesi</h1>
+      </div>
+
       <TravelSelection />
+
+      <div className="name-div">
+        <span>Atakan OGUZ</span>
+        <span>1191602092</span>
+      </div>
     </div>
   );
 };
